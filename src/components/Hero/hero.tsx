02717ffde2b50c8f790b9f0f02e2ef0022eea517
Andresa <andresa_15ga@hotmail.com>
2024-02-18 02:53:14 -0300

    chore: ajustando estilização do hero

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -10,8 +10,8 @@ export function Hero() {
     return (
         <div className="pt-24 max-md:pt-6 h-full max-md:w-full">
             <div className="grid grid-cols-2">
-                <div className="max-md:flex max-md:flex-col max-md:items-center max-md:justify-center mb-4 pt-28 max-md:pt-0 pl-44 max-md:pl-24 w-5/6 max-md:w-96">
-                    <ReactTyped strings={["Seus dados seguros e salvos conosco"]} typeSpeed={50} className="font-bold text-3xl max-md:text-xl mb-8 max-md:mb-2 text-violet-900 cormorant-garamond-bold uppercase " />
+                <div className="max-md:flex max-md:flex-col max-md:items-center max-md:justify-center mb-4 pt-28 max-md:pt-0 pl-44 max-md:px-14 w-5/6 max-md:w-96">
+                    <ReactTyped strings={["Seus dados 100% seguros conosco"]} typeSpeed={50} className="font-bold text-3xl max-md:text-xl mb-8 max-md:mb-2 text-violet-900 cormorant-garamond-bold uppercase" />
                     <p className='text-slate-500 text-center max-md:text-justify pt-8'>
                         Na SendData Company, a segurança e a privacidade dos dados dos usuários são nossa prioridade máxima. Trabalhamos incansavelmente para garantir a confiança e a satisfação de nossos clientes, proporcionando uma experiência segura e transparente em todas as interações com os dados dos usuários.
                     </p>
